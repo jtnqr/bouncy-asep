ab574cd8f3e9f2e72ee3c1bfe5c991ed1c0b8bb3
jtnqr <juliuspanjul25@gmail.com>
2024-05-17 15:40:02 +0700

    cleanup the code of main class

--- core/src/com/binaryneedle/bouncyasep/BouncyAsep.java
+++ core/src/com/binaryneedle/bouncyasep/BouncyAsep.java
@@ -16,20 +16,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BouncyAsep extends ApplicationAdapter {
-    Texture bucketImage;
-    Texture rainImage;
-    Background layer1, layer2, layer3;
-    Sound jumpSound;
-    SpriteBatch batch;
-    OrthographicCamera camera;
-    BitmapFont font;
-    MainEntity entity;
-    boolean isRunning, isColliding, isDebugEnabled;
+    private Texture bucketImage, rainImage;
+    private Background layer1, layer2, layer3;
+    private Sound jumpSound;
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private BitmapFont font;
+    private MainEntity entity;
+    private boolean isRunning, isColliding, isDebugEnabled, collision = true;
     private final int tileSquared = 64;
-    List<Obstacle> obstacles;
-    Character sprite;
-    boolean collision = true;
-    int score = 0;
+    private List<Obstacle> obstacles;
+    private Character sprite;
+    private int score;
 
     @Override
     public void create() {
@@ -39,6 +37,7 @@ public class BouncyAsep extends ApplicationAdapter {
         bucketImage = new Texture(Gdx.files.internal("bucket.png"));
         rainImage = new Texture(Gdx.files.internal("drop.png"));
         jumpSound = Gdx.audio.newSound(Gdx.files.internal("jump.mp3"));
+
         layer1 = new Background("bg/background_layer_1.png", 0.1f);
         layer2 = new Background("bg/background_layer_2.png", 125f);
         layer3 = new Background("bg/background_layer_3.png", 250f);
@@ -57,24 +56,40 @@ public class BouncyAsep extends ApplicationAdapter {
         );
 
         font = new BitmapFont();
-        obstacles = new ArrayList<Obstacle>();
+        font.setColor(Color.WHITE);
+        obstacles = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
             obstacles.add(new Obstacle(16f + (4f * i), 7, 5f, 250f, tileSquared));
         }
+
+        score = 0;
     }
+
 
     @Override
     public void render() {
         ScreenUtils.clear(0, 0, 0.2f, 1);
-
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
+
         layer1.draw(batch, Gdx.graphics.getHeight());
         layer2.draw(batch, Gdx.graphics.getHeight());
         layer3.draw(batch, Gdx.graphics.getHeight());
 
+        drawObstacles();
+        drawScore();
+        drawDebugInfo();
+        drawMenuText();
+
+        sprite.draw(batch, entity.getX(), entity.getY(), entity.getWidth(), entity.getHeight());
+        batch.end();
+
+        engineRun();
+    }
+
+    private void drawObstacles() {
         for (Obstacle obs : obstacles) {
             float obstacleX = obs.getTopPipe().getX();
             float topY = obs.getTopPipe().getY() / tileSquared;
@@ -111,10 +126,13 @@ public class BouncyAsep extends ApplicationAdapter {
 
             if (isRunning) obs.update(Gdx.graphics.getDeltaTime());
         }
+    }
 
-        font.setColor(Color.WHITE);
-        font.draw(batch, "Score: " + score, 1024 / 2f - 70, 768 - 50);
+    private void drawScore() {
+        font.draw(batch, "Score: " + score, 1024 / 2f - 35, 768 - 50);
+    }
 
+    private void drawDebugInfo() {
         if (isDebugEnabled) {
             String debugText = "Y: " + Math.round(entity.getY()) +
                     "\nVelocity: " + entity.getVelocity() +
@@ -122,57 +140,20 @@ public class BouncyAsep extends ApplicationAdapter {
                     "\nGravity: " + entity.getGravity();
             font.draw(batch, debugText, 10, 768 - 10);
         }
+    }
 
+    private void drawMenuText() {
         if (!isRunning) {
-            String menuText = "";
-            if (isColliding) {
-                menuText = "You crashed, press R to reset the game";
-            } else {
-                menuText = "Press SPACE or LMB to play the game";
-            }
+            String menuText = isColliding
+                    ? "GAME OVER, press R to reset the game"
+                    : "Press SPACE or LMB to play the game";
             font.draw(batch, menuText, 1024 / 2f - 140, 768 / 2f - 150);
         }
-
-        sprite.draw(batch, entity.getX(), entity.getY(), entity.getWidth(), entity.getHeight());
-
-        batch.end();
-        engineRun();
     }
 
     public void engineRun() {
-        if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
-            isColliding = false;
-            isRunning = false;
-            entity.setVelocity(0);
-            entity.setY(768 / 2f);
+        handleInput();
 
-            for (Obstacle obs : obstacles) {
-                obs.reset();
-            }
-            layer1.reset();
-            layer2.reset();
-            layer3.reset();
-
-            score = 0;
-        }
-        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
-        if (Gdx.input.isKeyJustPressed(Input.Keys.F12)) isDebugEnabled = !isDebugEnabled;
-        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_0)) collision = !collision;
-        if (!isColliding && Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) ||
-                Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-//            jumpSound.play();
-            entity.jump();
-        }
-
-        if (entity.getVelocity() != 0) isRunning = true;
-
-        if (collision) {
-            for (Obstacle obs : obstacles) {
-                if (obs.checkCollision(entity)) {
-                    isRunning = false;
-                }
-            }
-        }
         if (isRunning) {
             float deltaTime = Gdx.graphics.getDeltaTime();
 
@@ -183,12 +164,54 @@ public class BouncyAsep extends ApplicationAdapter {
             layer3.update(deltaTime);
         }
 
+        checkObstaclePass();
+    }
+
+    private void handleInput() {
+        if (Gdx.input.isKeyJustPressed(Input.Keys.R)) resetGame();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) Gdx.app.exit();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.F12)) isDebugEnabled = !isDebugEnabled;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_0)) collision = !collision;
+
+        if (Gdx.input.isButtonJustPressed(Input.Buttons.LEFT) ||
+                Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            entity.jump();
+        }
+
+        if (entity.getVelocity() != 0) isRunning = true;
+        if (collision) {
+            for (Obstacle obs : obstacles) {
+                if (obs.checkCollision(entity)) {
+                    isRunning = false;
+                    isColliding = true;
+                }
+            }
+        }
+    }
+
+    private void checkObstaclePass() {
         for (Obstacle obs : obstacles) {
             if (!obs.isPassed() && entity.getX() > obs.getTopPipe().getX() + obs.getTopPipe().getWidth()) {
                 obs.setPassed(true);
                 score++;
             }
         }
+    }
+
+    private void resetGame() {
+        isColliding = false;
+        isRunning = false;
+        entity.setVelocity(0);
+        entity.setY(768 / 2f);
+
+        for (Obstacle obs : obstacles) {
+            obs.reset();
+        }
+        layer1.reset();
+        layer2.reset();
+        layer3.reset();
+
+        score = 0;
     }
 
     @Override
