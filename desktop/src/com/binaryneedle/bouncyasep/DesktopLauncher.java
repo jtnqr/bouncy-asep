eae662f7645cfbf0ac772e1ec218e55387b5cb91
jtnqr <juliuspanjul25@gmail.com>
2024-05-21 13:19:37 +0700

    i really disapprove for this kind of logo but ok

--- desktop/src/com/binaryneedle/bouncyasep/DesktopLauncher.java
+++ desktop/src/com/binaryneedle/bouncyasep/DesktopLauncher.java
@@ -2,17 +2,17 @@ package com.binaryneedle.bouncyasep;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.binaryneedle.bouncyasep.BouncyAsep;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         config.setWindowedMode(1024, 768);
-//        config.setForegroundFPS(165);
+        config.setForegroundFPS(60);
         config.useVsync(false);
         config.setResizable(false);
         config.setTitle("Bouncy Asep: The Game");
+        config.setWindowIcon("icons/logo.jpg");
         new Lwjgl3Application(new BouncyAsep(), config);
     }
 }
